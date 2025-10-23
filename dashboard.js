5134558e6458a4b0fb4e0287feb198a3e403424a
peterberkeley <peter.berkeley@inalgesco.com>
2025-10-23 17:51:01 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -131,10 +131,12 @@ async function resolveDeviceTz(deviceLabel, lat, lon){
 }
 
 /* =================== Admin mapping (context) =================== */
+async function fetchSensorMapMapping(){
+  try{
     const res = await fetch(`${UBIDOTS_V1}/devices/config/sensor_map/values?page_size=1&_=${Date.now()}&token=${UBIDOTS_ACCOUNT_TOKEN}`);
-    const js = await res.json();
+    const js  = await res.json();
     sensorMapConfig = (js.results?.[0]?.context) || {};
-    aliasMap = sensorMapConfig.__aliases || {};
+    aliasMap        = sensorMapConfig.__aliases || {};
   }catch(e){
     console.error("Failed to fetch sensor_map:", e);
     sensorMapConfig = {};
